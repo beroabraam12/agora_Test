7efe19ecf49edea6b0df7795715825a5cebd5c4c
beroabraam12 <ebraamboshra7@gmail.com>
2020-07-02 23:07:09 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/agore_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.agore_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,2 @@
+// Agora AppId
+const APP_ID = '09750d9e93124d4f978d7c5a4c651bae';
